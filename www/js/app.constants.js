2db8cf983d9abcf09d86b1e84f19a49f3d17630e
gowthamhosur <hosur.gowtham@gmail.com>
2017-05-03 22:07:03 +0100

    Added puzzles

--- www/js/app.constants.js
+++ www/js/app.constants.js
@@ -5,7 +5,7 @@
 gameModule.constant('gameConstants', {
 	language: 'telugu',
 	initialLevel: 0,
-	totalLevels: 13,
+	totalLevels: 19,
 	numberOfPics: 4,
 	maxChoosableLetters: 8,
 	totalGameLevels: 50
