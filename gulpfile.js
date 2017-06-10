eb9952ba01baf115836bcd531c8957201c193f9a
gowthamhosur <hosur.gowtham@gmail.com>
2017-06-10 15:37:18 +0100

    Removed unnecessary files in www/lib

--- gulpfile.js
+++ gulpfile.js
@@ -82,20 +82,8 @@ gulp.task('images',function() {
 
 gulp.task('solutions', function(){
   gulp.src('./puzzles/**/*.json')
-      .pipe(merge({
-        fileName: 'solutions-raw.json',
-        edit: function (parsedJson, file){
-          var folder = path.basename(path.dirname(file.path))
-
-          Object.keys(parsedJson).forEach(function(language) {
-            parsedJson[language][folder] = parsedJson[language]["answer"];
-            delete parsedJson[language]["answer"]
-          });
-            return parsedJson;
-        }
-      }))
        .pipe(merge({
-        fileName: 'solutions-raw.json',
+        fileName: 'solutions.json',
         edit: function (parsedJson, file){
           var folder = path.basename(path.dirname(file.path))
           Object.keys(parsedJson).forEach(function(language) {
